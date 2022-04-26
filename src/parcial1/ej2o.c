67105b60f2fd125746f232309863b64c1a970941
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2022-04-26 18:20:52 -0300

    Optimizar cálculo de índices

--- src/parcial1/ej2o.c
+++ src/parcial1/ej2o.c
@@ -10,8 +10,8 @@ int main()
     int v_offset;
 
     __m256i v1, v2, v3;
-    __m256i iaux1, iaux2, iaux3;
-    __m256i daux1, daux2;
+    __m256i aux1, aux2;
+    __m256i iaux1, iaux2;
     __m256i cmp1;
 
     offset = 256 / (8 * sizeof(int));
@@ -28,32 +28,30 @@ int main()
     printf("v[11]=%11d, ", v[11]);
     printf("v[elementos-1]=%11d\n", v[elementos-1]);
 
-    daux1 = _mm256_set1_epi32(2);
-    daux2 = _mm256_set1_epi32(-2);
-    iaux2 = _mm256_set1_epi32(10);
-    iaux3 = _mm256_set_epi32(0, 1, 2, 3, 4, 5, 6, 7); //i offsets
+    aux1 = _mm256_set1_epi32(2);
+    aux2 = _mm256_set1_epi32(-2);
+    iaux1 = _mm256_set_epi32(0, 10, 20, 30, 40, 50, 60, 70); //i*10 start
+    iaux2 = _mm256_set1_epi32(80); //i*10 step
 
     for (i = 0; i < iters; i++) {
         v_offset = i * offset;
 
-        //cargar vector
+        // cargar vector
         v1 = _mm256_load_si256((__m256i const *)&v[v_offset]);
 
-        //computar vector <(i+0)*10, ..., (i+7)*10>
-        iaux1 = _mm256_set1_epi32(v_offset);
-        iaux1 = _mm256_add_epi32(iaux1, iaux3);
-        iaux1 = _mm256_mullo_epi32(iaux1, iaux2);
-
-        cmp1 = _mm256_cmpgt_epi32(iaux1, v1); // v[i] <  i*10
-        v3 = _mm256_and_si256(cmp1, daux1);
-        v2 = _mm256_andnot_si256(cmp1, daux2);
+        // realizar comparación: v[i] < i*10
+        cmp1 = _mm256_cmpgt_epi32(iaux1, v1);
+        v3 = _mm256_and_si256(cmp1, aux1);
+        v2 = _mm256_andnot_si256(cmp1, aux2);
         v2 = _mm256_or_si256(v2, v3);
 
-        //computar resultado
+        // computar resultado
         v1 = _mm256_mullo_epi32(v1, v2);
 
-        //guardar resultado
-        _mm256_store_si256((__m256i *)&v[v_offset], v1); // v = v1
+        // guardar resultado
+        _mm256_store_si256((__m256i *)&v[v_offset], v1);
+
+        iaux1 = _mm256_add_epi32(iaux1, iaux2);
     }
 
     printf("v[0]=%11d, ", v[0]);
