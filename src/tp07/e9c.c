dd0220d3540a9514df2f321b3e00e65d28c81d7b
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2022-06-16 21:53:44 -0300

    Agregado ej. 9b y 9c

--- src/tp07/e9c.c
+++ src/tp07/e9c.c
@@ -9,8 +9,13 @@ int main(int argc, char **argv)
     int *vec_a, *vec_b;
     int i, x = 0;
     double t;
+
     enum PROCESSES {PROC_A, PROC_B};
+
+    MPI_Status send_st;
     MPI_Status recv_st;
+    MPI_Request send_req;
+    MPI_Request recv_req;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -27,16 +32,17 @@ int main(int argc, char **argv)
     switch (rank) {
         case PROC_A:
             dest = PROC_B;
-            MPI_Send(vec_a, N, MPI_INT, dest, 0, MPI_COMM_WORLD);
-            MPI_Recv(vec_b, N, MPI_INT, dest, 0, MPI_COMM_WORLD, &recv_st);
             break;
 
         case PROC_B:
             dest = PROC_A;
-            MPI_Recv(vec_b, N, MPI_INT, dest, 0, MPI_COMM_WORLD, &recv_st);
-            MPI_Send(vec_a, N, MPI_INT, dest, 0, MPI_COMM_WORLD);
             break;
     }
+
+    MPI_Isend(vec_a, N, MPI_INT, dest, 0, MPI_COMM_WORLD, &send_req);
+    MPI_Irecv(vec_b, N, MPI_INT, dest, 0, MPI_COMM_WORLD, &recv_req);
+    MPI_Wait(&send_req, &send_st);
+    MPI_Wait(&recv_req, &recv_st);
 
     for (i = 0; i < N; i++)
         vec_a[i] = x + vec_b[i];
