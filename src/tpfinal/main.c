b6d2c2275b3a5cde89d14f5f291dab0cdf5ff3b7
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2022-06-20 19:25:49 -0300

    Actualizado topología TPFinal

--- src/tpfinal/main.c
+++ src/tpfinal/main.c
@@ -35,15 +35,16 @@ int main(int argc, char **argv)
     //columnas auxiliares
     nrows = rows + 2;
     ncols = cols + 2;
+    printf("Rows: %d cols: %d\n", rows, cols);
 
     //MPI----------------------------------------------------------------------
-    int size, rank;
+    int size;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     //Permitir solo cantidad de procesos pares
-    if (size < 1 || (size % 2) != 0) {
+    if (size < 2 || (size % 2) != 0) {
         printf("Error: debe ejecutarse con un número par de procesos\n");
         MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     }
@@ -53,15 +54,49 @@ int main(int argc, char **argv)
     int periods[2] = {true, true};
     int reorder = false;
     MPI_Comm new_comm;
+
     MPI_Dims_create(size, 2, dims);
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &new_comm);
+    printf("Dimensiones: %d, %d\n", dims[0], dims[1]);
+
+    //Obtener datos del proceso
+    int rank;
+    int coords[2];
+    int next_ranks[4];
+    char* next_names[4] = {"up", "down", "left", "right"};
+    enum DIRECTIONS {UP, DOWN, LEFT, RIGHT};
 
     MPI_Comm_rank(new_comm, &rank);
-
-    int coords[2];
     MPI_Cart_coords(new_comm, rank, 2, coords);
-
+    MPI_Cart_shift(new_comm, 0, 1, &next_ranks[UP], &next_ranks[DOWN]);
+    MPI_Cart_shift(new_comm, 1, 1, &next_ranks[LEFT], &next_ranks[RIGHT]);
     printf("[MPI process %d] Coords: (%d, %d).\n", rank, coords[0], coords[1]);
+    printf("[MPI process %d] Neighbours:", rank);
+    for (i = 0; i < 4; i++)
+        printf(" %s: %d;", next_names[i], next_ranks[i]);
+    printf("\n");
+
+    //Repartir datos
+    int chunk_lengths[2];
+    int chunk_remains[2];
+    enum DIMENSIONS {ROWS, COLS};
+    chunk_lengths[ROWS] = rows / dims[ROWS];
+    chunk_lengths[COLS] = cols / dims[COLS];
+    chunk_remains[ROWS] = rows % dims[ROWS];
+    chunk_remains[COLS] = cols % dims[COLS];
+
+    //Asignar resto a última fila y columna
+    if (coords[0] == (dims[0]-1) && chunk_remains[ROWS] != 0)
+            chunk_lengths[ROWS] += chunk_remains[ROWS];
+    if (coords[1] == (dims[1]-1) && chunk_remains[COLS] != 0)
+        chunk_lengths[COLS] += chunk_remains[COLS];
+
+    //Crear tipo de datos para la porción de datos
+    MPI_Datatype chunk_col_type;
+    MPI_Type_vector(cols, chunk_lengths[COLS], cols, MPI_CHAR, &chunk_col_type);
+    MPI_Type_commit(&chunk_col_type);
+
+    printf("[MPI process %d] Chunk col length: (%d, %d).\n", rank, chunk_lengths[ROWS], chunk_lengths[COLS]);
 
     MPI_Finalize();
 
